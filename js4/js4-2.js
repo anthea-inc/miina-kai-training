a03481a8d5be55af2c57a0cee5340dfb6dc62b65
KaiMiina <miiiii1818@icloud.com>
2025-02-05 14:35:29 +0900

    課題2もう一個あった

--- js4/js4-2.js
+++ js4/js4-2.js
@@ -2,3 +2,7 @@ let x = "3.14";
 
 let myNumber = Number(x);
 console.log(typeof myNumber, myNumber);
+
+let myParseInt = parseInt(x);
+console.log(typeof myParseInt, myParseInt);
+// 結果何になれば正解かわからない。
