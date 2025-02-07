adb0599e559fd244896c8d471f37fb27a8eaaa58
KaiMiina <miiiii1818@icloud.com>
2025-02-07 11:00:41 +0900

    実践課題1完了

---
+++ js6/comparison.js
@@ -0,0 +1,14 @@
+const a = 5;
+const b = "5";
+
+console.log(a == b);
+console.log(a === b);
+
+console.log(a != b);
+console.log(a !== b);
+
+console.log(a > 3);
+console.log(a >= 5);
+
+console.log(a < 10);
+console.log(a <= 5);
---
+++ js6/logical.js
@@ -0,0 +1,13 @@
+const andExample = 10 > 5 && 5 > 2;
+console.log(andExample);
+
+const orExample = 10 < 5 || 5 > 2;
+console.log(orExample);
+
+const notExample = !(10 > 5);
+console.log(notExample);
+
+console.log("Hello" || "デフォルト値");
+console.log("" || "デフォルト値");
+console.log(0 || "デフォルト値");
+console.log(null || "デフォルト値");
