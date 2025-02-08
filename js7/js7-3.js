a8c04ba066a09a78b85aaf9116946fb74b445432
KaiMiina <miiiii1818@icloud.com>
2025-02-08 10:50:44 +0900

    とりあえず何も見ずに作成

---
+++ js7/js7-3.js
@@ -0,0 +1,28 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("1から7の整数を入力してください: ", (input) => {
+  let day = Number(input);
+
+  if (day === 1) {
+    console.log("月曜日");
+  } else if (day === 3) {
+    console.log("火曜日");
+  } else if (day === 4) {
+    console.log("水曜日");
+  } else if (day === 5) {
+    console.log("木曜日");
+  } else if (day === 6) {
+    console.log("金曜日");
+  } else if (day === 7) {
+    console.log("土曜日");
+  } else {
+    console.log("無効な入力です");
+  }
+
+  rl.close();
+});
