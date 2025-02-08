b9649a27bc0dd87587c9a52e1f93475553d83b01
KaiMiina <miiiii1818@icloud.com>
2025-02-08 11:50:37 +0900

    変更

--- js7/js7-3.js
+++ js7/js7-3.js
@@ -8,23 +8,30 @@ const rl = readline.createInterface({
 rl.question("1から7の整数を入力してください: ", (input) => {
   let day = Number(input);
 
-  if (day === 1) {
-    console.log("月曜日");
-  } else if (day === 2) {
-    console.log("火曜日");
-  } else if (day === 3) {
-    console.log("水曜日");
-  } else if (day === 4) {
-    console.log("木曜日");
-  } else if (day === 5) {
-    console.log("金曜日");
-  } else if (day === 6) {
-    console.log("土曜日");
-  } else if (day === 7) {
-    console.log("日曜日");
-  } else {
-    console.log("無効な入力です");
+  switch (day) {
+    case 1:
+      console.log("月曜日");
+      break;
+    case 2:
+      console.log("火曜日");
+      break;
+    case 3:
+      console.log("水曜日");
+      break;
+    case 4:
+      console.log("木曜日");
+      break;
+    case 5:
+      console.log("金曜日");
+      break;
+    case 6:
+      console.log("土曜日");
+      break;
+    case 7:
+      console.log("日曜日");
+      break;
+    default:
+      console.log("無効な入力です");
   }
-
   rl.close();
 });
