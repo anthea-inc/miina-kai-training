3e251003d22eb5bdbab2c9df40df8c816f4d7574
KaiMiina <miiiii1818@icloud.com>
2025-02-16 14:13:06 +0900

    まだ、答え合わせ前

---
+++ js9/js9-1/js9-1.js
@@ -0,0 +1,14 @@
+const person = {
+  id: 8,
+  name: "Miina",
+  "job-title":"Engineer"
+  "birth date":"20020818"
+  address: { 
+    city: "Hokkaido", 
+    country: "Japan"
+  }
+  
+};
+const person = { id, name, "job-title", "birth date"};
+
+console.log(person);
