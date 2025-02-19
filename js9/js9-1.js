d17bcb90384046d3a9489dd407f9d75d222df3db
KaiMiina <miiiii1818@icloud.com>
2025-02-19 14:03:24 +0900

    4-1完了

--- js9/js9-1.js
+++ js9/js9-1.js
@@ -12,14 +12,22 @@ const person = {
   },
 };
 
+console.log("-----------ステップ2-1--------------");
+
 console.log(person.name);
 console.log(person.address.city);
+
+console.log("-----------ステップ2-2--------------");
 
 console.log(person["birth date"]);
 console.log(person["job-title"]);
 
+console.log("-----------ステップ2-3--------------");
+
 console.log(person?.from);
 console.log(person.address?.phone);
+
+console.log("-----------ステップ2-4--------------");
 
 const newPropKey = "hobby";
 const newPropValue = 5;
@@ -27,6 +35,39 @@ const newPropValue = 5;
 person[newPropKey] = newPropValue;
 console.log(person);
 
+console.log("-----------ステップ3-1--------------");
 person.namae = "Kai";
 
 console.log(person);
+
+console.log("-----------ステップ3-2--------------");
+
+delete person["birth date"];
+console.log(person["birth date"]);
+
+console.log("-----------ステップ3-3--------------");
+
+const frozenPerson = {
+  food: "Sushi",
+  Number: 317,
+  country: "Japan",
+};
+
+Object.freeze(frozenPerson);
+
+frozenPerson.food = "Ramen";
+console.log(frozenPerson);
+
+console.log("-----------ステップ3-4--------------");
+
+console.log("id" in person); // in 演算子
+
+console.log(Object.hasOwn(person, "id")); // Object.hasOwn
+
+console.log(person.hasOwnProperty("id")); // Object.prototype.hasOwnProperty
+
+console.log("-----------ステップ4-1--------------");
+
+console.log(Object.keys(frozenPerson));
+console.log(Object.values(frozenPerson));
+console.log(Object.entries(frozenPerson));
