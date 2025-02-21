bba88216c3e072dbda2814a258567411f7ab88ec
KaiMiina <miiiii1818@icloud.com>
2025-02-21 14:58:52 +0900

    遠いな。。

--- js10/js10.js
+++ js10/js10.js
@@ -1,3 +1,4 @@
-const fruits = ["りんご", "バナナ", "ぶどう"];
-fruits.shift();
-console.log(fruits);
+const flatArray = [1, 2, [3, 4, [5, 6]]];
+
+const flattened = flatArray.flat(2); // 1階層だけ展開
+console.log(flattened);
