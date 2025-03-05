b4094a2e1f5ccb208f7334a843a13551862ce255
KaiMiina <miiiii1818@icloud.com>
2025-03-05 11:17:25 +0900

    知識課題完了

--- js11/js11.js
+++ js11/js11.js
@@ -1,2 +1,6 @@
-const text = "子持ち ししゃも";
-console.log(text.length);
+const text = "今日のご飯はししゃもです。ししゃもは嫌ですね。";
+
+// ししゃも全てを「さば」に置換
+const newText = text.replaceAll("ししゃも", "さば");
+
+console.log(newText); // 今日のご飯はさばです。さばは嫌ですね。
