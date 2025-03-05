965ca28dbe47a992a05cc188e7bb42812e7e789e
KaiMiina <miiiii1818@icloud.com>
2025-03-05 12:40:28 +0900

    後一問、実践課題3の理解が難しい

--- js11/js11.js
+++ js11/js11.js
@@ -1,6 +1,41 @@
-const text = "今日のご飯はししゃもです。ししゃもは嫌ですね。";
+console.log("------------知識課題1------------");
+console.log(
+  "こんにちは！\tお昼のニュースをお伝えします。\n研修生のみいなさんは\"JavaScript\" の勉強をしており。彼女は'難しいですが楽しいです。'と発言していたことが判明しました。"
+);
+console.log("------------知識課題2------------");
+const str = "JavaScript";
+console.log(str[0]);
+console.log(str.at(0));
+console.log("------------知識課題3-1------------");
+const csv = "apple,banana,cherry";
+const fruits = csv.split(",");
+console.log(csv);
+console.log("------------知識課題3-2------------");
+const csv2 = "apple,banana,cherry";
 
-// ししゃも全てを「さば」に置換
-const newText = text.replaceAll("ししゃも", "さば");
+console.log(csv2.split);
 
-console.log(newText); // 今日のご飯はさばです。さばは嫌ですね。
+console.log("------------知識課題4------------");
+const text = "子持ち ししゃも!";
+console.log(text.length);
+const tex = "";
+console.log(tex.length);
+
+console.log("------------知識課題5------------");
+const str1 = "JavaScript is fun!";
+console.log(str1.slice(0, 11));
+console.log(str1.substring(10));
+
+console.log("------------知識課題6------------");
+const str2 = "I love JavaScript and JavaScript loves me.";
+console.log(str2.indexOf("JavaScript"));
+console.log(str2.lastIndexOf("JavaScript"));
+console.log(str2.startsWith("I love"));
+console.log(str2.endsWith("me."));
+console.log(str2.includes("and"));
+
+console.log("------------知識課題7------------");
+const str3 = "I like cats. Cats are very cute. I have a cat.";
+
+console.log(str3.replace("cat", "dog"));
+console.log(str3.replaceAll("cat", "dog"));
