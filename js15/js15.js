c42e8681c8dc7655df0cc84a8c3f1e8432176782
KaiMiina <miiiii1818@icloud.com>
2025-03-21 04:03:34 +0900

    楽しい

--- js15/js15.js
+++ js15/js15.js
@@ -1,18 +1,44 @@
-// クラスの定義
-class Car {
-  constructor(brand, color) {
-    this.brand = brand; // 車のブランド
-    this.color = color; // 車の色
+class user {
+  name;
+  age;
+
+  #password; // # をつけることでprivateフィールドを定義可能
+
+  constructor(name, age, password) {
+    // User クラスをインスタンス化する時、この3つを渡す
+    // 静的プロパティ
+    this.name = name; // インスタンスのプロパティを初期化
+    this.age = age; // インスタンスのプロパティを初期化
+    this.#password = password; // Privateフィールドを初期化
+
+    User.userCount++; // ユーザーが作られるたびにカウントアップされるみたい
   }
 
-  // メソッド（動作を定義）
-  drive() {
-    console.log(`${this.color} の ${this.brand} が走っています！`);
+  set password(newPassword) {
+    // setterメゾット
+    if (newPassword.length >= 6) {
+      this.#password = newPassword;
+      console.log("チェック完了"); //6文字以上の場合実行
+    } else {
+      console.log("6文字未満だよ"); // 6文字以下の場合実行
+    } //  三項演算子でも書けるよね。
+  }
+
+  get password() {
+    // getterメゾット
+    // getterはgetで記載する。
+    return "******"; // マスクした文字列を返すようにしてる
+  }
+
+  greet() {
+    //  プロトタイプメソッド /greet()メソッド
+    console.log(
+      `こんにちは！ ${this.name} さん。年齢は ${this.age}歳ですね！ `
+    ); //え、こうゆうこと？
+  }
+
+  static getUserCount() {
+    return `現在のユーザー数: ${User.userCount} 人`;
   }
 }
-// `new` を使ってインスタンスを生成
-const myCar = new Car("Toyota", "赤");
-const yourCar = new Car("Honda", "青");
-// インスタンスのメソッドを呼び出す
-myCar.drive(); // "赤 の Toyota が走っています！"
-yourCar.drive(); // "青 の Honda が走っています！"
+// 名前定義してないから何も出ないんじゃね？
