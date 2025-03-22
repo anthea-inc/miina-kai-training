f35cf09e5b47825d02ac0a1ab8fdd9e405233604
KaiMiina <miiiii1818@icloud.com>
2025-03-23 00:10:10 +0900

    一旦

--- js16/js16.js
+++ js16/js16.js
@@ -1,13 +1,3 @@
-function checkAge(age) {
-  if (age < 0) {
-    // 年齢がマイナスなんてありえない！エラーを投げよう！
-    throw new Error("年齢は0以上である必要があります");
-  }
-  console.log(`年齢は ${age} 歳です`);
-}
+try {process.argvslice(2)
 
-try {
-  checkAge(178); // ← ここでエラーをわざと発生させる
-} catch (e) {
-  console.error("エラーが発生しました:", e.message);
 }
