d7c384c93207aaf1f407d9847a274b9446fed63b
KaiMiina <miiiii1818@icloud.com>
2025-03-23 00:51:20 +0900

    げんかい

--- js16/js16.js
+++ js16/js16.js
@@ -1,3 +1,16 @@
-try {process.argvslice(2)
-
+const num1 = process.argv[2];
+const num2 = process.argv[3];
+//ifを使いところまでは理解している。
+console.log(num1);
+console.log(num2);
+try {
+  if (isNaN(num1) || isNaN(num2)) {
+    throw new Error("これは数値ではない");
+  }
+  if (num1 === 0) {
+    // 0での割り算はエラー
+    throw new Error("0で割ることはできません");
+  }
+} catch (error) {
+  console.error("エラーが発生");
 }
