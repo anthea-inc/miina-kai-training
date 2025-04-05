4d4b3c96c87bacb1f6e22daf0d878777093f801f
KaiMiina <miiiii1818@icloud.com>
2025-04-05 09:18:28 +0900

    完了

--- react4/miina-app/src/App.jsx
+++ react4/miina-app/src/App.jsx
@@ -1,12 +1,16 @@
-import SimpleList from "./SimpleList";
+import ItemList from "./ItemList";
 
 function App() {
-  const fruitList = ["リンゴ", "バナナ", "チェリー"];
+  const fruits = [
+    { id: "a1", name: "リンゴ" },
+    { id: "b2", name: "バナナ" },
+    { id: "c3", name: "チェリー" },
+  ];
 
   return (
     <div>
       <h1>フルーツ一覧</h1>
-      <SimpleList fruits={fruitList} />
+      <ItemList items={fruits} />
     </div>
   );
 }
---
+++ react4/miina-app/src/ItemList.jsx
@@ -0,0 +1,11 @@
+function ItemList({ items }) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ItemList;
