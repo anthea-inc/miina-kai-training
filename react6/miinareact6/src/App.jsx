75410f1cc0d71ec2b01ded28b2b840a8a6c9df32
KaiMiina <miiiii1818@icloud.com>
2025-04-09 10:10:59 +0900

    実践課題1

---
+++ react6/miinareact6/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./Counter"; // カウントコンポーネントを読み込み
+
+function App() {
+  return (
+    <div>
+      <h1>Reactカウントアプリ</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
