93c7dd0488e738d8b7226970cb88d59ef20b4d3c
KaiMiina <miiiii1818@icloud.com>
2025-04-12 14:13:08 +0900

    実践課題1

---
+++ react10/miinareact10/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Button.css";
+
+function Button() {
+  return <button className="my-button">ボタン</button>;
+}
+export default Button;
+//	CSSファイルを import して使えるようにしてる
+// 	className="my-button" で、CSSで定義したクラスを当てている
