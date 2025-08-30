6bf67a83696c6bc8091134e84d9243167f9242fe
KaiMiina <miiiii1818@icloud.com>
2025-08-30 18:06:58 +0900

    GETとPOSTリクエストを受け取るサーバーを作成

--- http-server-intro/server.js
+++ http-server-intro/server.js
@@ -1,8 +1,25 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, Node.js!");
+  const headers = {
+    "Content-Type": "text/plain",
+  };
+
+  // リクエストのメソッドによって処理を分岐
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, World!\n");
+  } else if (req.method === "POST") {
+    res.writeHead(200, headers);
+    res.end("Received POST reqest\n");
+  } else {
+    res.writeHead(404, headers);
+    res.end("404 Not Found\n");
+  }
 });
 
-server.listen(3000);
+// サーバーを起動
+
+server.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
